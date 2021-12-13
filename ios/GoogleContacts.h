a55ba9ec9a3c6ae1504867192fabdf84874eff86
Vasanthvip8 <vasanthvip8@gmail.com>
2021-12-13 10:48:43 +0530

    latest code

--- ios/GoogleContacts.h
+++ ios/GoogleContacts.h
@@ -1,9 +1,7 @@
 #import <React/RCTBridgeModule.h>
 #import <AppAuth/AppAuth.h>
 #import <GTMAppAuth/GTMAppAuth.h>
-@import GoogleSignIn;
-//@import AppAuth;
-//@import GTMAppAuth;
+
 @interface GoogleContacts : NSObject <RCTBridgeModule>
 @property(nonatomic, nullable)
     id<OIDExternalUserAgentSession> currentAuthorizationFlow;
